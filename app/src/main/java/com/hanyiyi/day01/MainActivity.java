c59d8c591e6cc35406a891d9f1ba51b1355506dc
Lucifer.Han <214644001@qq.com>
2016-06-27 11:30:06 +0800

    1 新增了一个TextView的id 2 更新了该TextView的内容

--- app/src/main/java/com/hanyiyi/day01/MainActivity.java
+++ app/src/main/java/com/hanyiyi/day01/MainActivity.java
@@ -2,12 +2,19 @@ package com.hanyiyi.day01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.tv_show);
+        mTextView.setText("我的文字被改变了");
+
     }
 }
